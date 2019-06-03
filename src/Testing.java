c0987e10f606f8e528458f1eba36e9de78650adc
Katie Jergens <katie.jergens@gmail.com>
2019-06-02 23:40:18 -0400

    Initialize commit

---
+++ src/Testing.java
@@ -0,0 +1,9 @@
+import org.dalton.Assignment;
+
+@Assignment(assignment = "testing")
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+        System.out.printf("");
+    }
+}
